22b90032bfb50baf0be5bd3a86298a310c16588c
Alamgir Hossain <alamgir555hossain@gmail.com>
2019-11-12 19:48:42 -0500

    initial commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About(){
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is the TOdoList App V1.0.0.0 It is part</p>
+        </React.Fragment>
+    )
+}
+
+export default About;
